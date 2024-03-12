76b60642572f31f382ec1c964e7376ddc28aef54
shadow <lightshadow416@gmail.com>
2024-03-12 20:22:43 +0100

    [Navbar] component was created

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as MyContainer } from "./MyContainer/MyContainer";
 export { default as MySidebar } from "./MySidebar/MySidebar";
+export { default as Navbar } from "./Navbar/Navbar";
