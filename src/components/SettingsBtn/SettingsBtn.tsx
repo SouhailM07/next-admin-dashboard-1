4ab5fdbcd733d8aba16dbb80a168e9de11707115
shadow <lightshadow416@gmail.com>
2024-03-14 00:47:33 +0100

    fixing style problems

--- src/components/SettingsBtn/SettingsBtn.tsx
+++ src/components/SettingsBtn/SettingsBtn.tsx
@@ -13,7 +13,7 @@ export default function SettingsBtn() {
       <div
         role="button"
         aria-label="settings button"
-        className={`themeIndex-${themeIndex} z-[99] text-[1.7rem] w-[2.5rem] h-[2.5rem] grid place-items-center rounded-full fixed bottom-[1rem] right-[1.5rem] hover:rotate-[120deg] transition-all duration-200 text-white`}
+        className={`themeIndex-${themeIndex} z-[10] text-[1.7rem] w-[2.5rem] h-[2.5rem] grid place-items-center rounded-full fixed bottom-[1rem] right-[1.5rem] hover:rotate-[120deg] transition-all duration-200 text-white`}
       >
         <FontAwesomeIcon icon={faGear} className="h-[1.5rem] w-[1.5rem]" />
       </div>
